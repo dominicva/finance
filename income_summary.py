2241a30c995aa709aba9a23d994414d86a59e5b2
Dominic van Almsick <dominicvana@gmail.com>
2021-01-03 07:11:07 -0500

    Label income_summary.py as WIP

--- income_summary.py
+++ income_summary.py
@@ -1,12 +1,15 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
-from secrets import IEX_CLOUD_API_TOKEN_SANDBOX
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import requests
+# from secrets import IEX_CLOUD_API_TOKEN_SANDBOX
+#
+#
+# symbol = 'asml'
+# date_range = '5y'
+# request_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/earnings{date_range}?period=annual?token={IEX_CLOUD_API_TOKEN_SANDBOX}'
+#
+# data = requests.get(request_url).json()
 
 
-symbol = 'asml'
-date_range = '5y'
-request_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/earnings{date_range}?period=annual?token={IEX_CLOUD_API_TOKEN_SANDBOX}'
-
-data = requests.get(request_url).json()
+"""WORK IN PROGRESS"""
